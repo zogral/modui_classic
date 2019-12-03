255d340fd4544b780ec1a9adf5dc5459d3964f21
zogral <>
2019-12-03 18:48:30 +0100

    hide unitframe stuff

--- unit/unit.lua
+++ unit/unit.lua
@@ -456,5 +456,22 @@
     for _, v in pairs(events) do e:RegisterEvent(v) end
     e:SetScript('OnEvent', OnEvent)
 
+    hooksecurefunc("PlayerFrame_UpdateStatus", function()
+        if IsResting("player") then
+            PlayerStatusTexture:Hide()
+            PlayerRestIcon:Hide()
+            PlayerRestGlow:Hide()
+            PlayerStatusGlow:Hide()
+        elseif PlayerFrame.inCombat then
+            PlayerStatusTexture:Hide()
+            PlayerAttackIcon:Hide()
+            PlayerRestIcon:Hide()
+            PlayerAttackGlow:Hide()
+            PlayerRestGlow:Hide()
+            PlayerStatusGlow:Hide()
+            PlayerAttackBackground:Hide() 
+        end 
+    end)
+
 
     --
